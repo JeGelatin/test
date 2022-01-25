4fbd69c9d7eafc13aad4e1786dd914ac0768e687
JeGelatin <thiti0930461536@gmail.com>
2022-01-25 13:37:06 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kiiki</title>
+</head>
+
+<body>
+    <?php
+    echo   "สวัสดีทุกท่าน<br/>";
+    echo    "หาผลรวมของ 5+5=",5+5;
+    echo  "<br>";
+    $a=20;   $b=5;
+
+     echo "ผลต่างของ $a กับ $b คือ", $a-$b;
+    ?>
+
+</body>
+
+</html>
